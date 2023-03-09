6bbb65f505e356567069a93f2c658982d15e8cba
P <dpheeraphat2547@gmail.com>
2023-03-09 20:27:47 +0700

    move to folder

--- about.js
+++
@@ -1,6 +0,0 @@
-let mainNav = document.getElementById('js-menu');
-let navBarToggle = document.getElementById('js-nav-toggle');
-
-navBarToggle.addEventListener("click", function(){
-     mainNav.classList.toggle('active');
-});
